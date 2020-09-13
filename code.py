db2f10a01310e6df315b5da7ff65f3d9fd3fbd94
anjaliagrawal <70996613+anjaliagrawal28@users.noreply.github.com>
2020-09-13 14:28:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 def convert():
-    img=Image.open('Anjali Agrawal.jpg')
+    img=Image.open('IMG-20181007-WA0060.jpg')
     text=pytesseract.image_to_string(img)
     print(text)
 
